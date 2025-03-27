2c3f7d49999263ff1a105186751099ebb05ab45c
Praveen S R <praveentiger1997@gmail.com>
2025-03-27 09:40:01 +0530

    upto jwt token creation

---
+++ WebAPIDemo/Authority/AppRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace WebAPIDemo.Authority
+{
+    public class AppRepository
+    {
+        private static List<Application> applications = new List<Application>
+        {
+            new Application
+            {
+                ApplicationId = 1,
+                ApplicationName = "MyWebApp",
+                ClientId = "E551DE03-F774-4FD5-81ED-81CBCC3C5A0A",
+                Secret = "E5ABCED0-48F2-4CA3-839D-C15B4CBF64A4",
+                Scopes = "read,write"
+            }
+        };
+
+        public static bool Authenticate(string clientId, string secret)
+        {
+            return applications.Any(x => x.ClientId == clientId && x.Secret == secret);
+        }
+
+        public static Application? GetApplication(string clientId)
+        {
+            return applications.FirstOrDefault(x => x.ClientId == clientId);
+        }
+    }
+}
---
+++ WebAPIDemo/Controllers/AuthorityController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebAPIDemo.Authority;
+
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    public class AuthorityController : ControllerBase
+    {
+        public AuthorityController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        [HttpPost("auth")]
+        public IActionResult Authenticate([FromBody] AppCredential credential)
+        {
+            if (AppRepository.Authenticate(credential.ClientId, credential.Secret))
+            {
+                var expiry = DateTime.UtcNow.AddMinutes(10);
+                return Ok(new
+                {
+                    access_token = CreateToken(credential.ClientId, expiry),
+                    expiresAt = expiry
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Unauthorized", "You are not authorised to access the site.");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status401Unauthorized,
+                };
+                return new UnauthorizedObjectResult(problemDetails);
+            }
+                
+        }
+
+        private string CreateToken(string clientId,DateTime expiry)
+        {
+            var app = AppRepository.GetApplication(clientId);
+            var claims = new List<Claim> 
+            {
+                new Claim("AppName",app.ApplicationName??string.Empty),
+                new Claim("Read",(app?.Scopes??string.Empty).Contains("read")?"true":"false"),
+                new Claim("Write",(app?.Scopes??string.Empty).Contains("write")?"true":"false"),
+            };
+            var secretKey = Encoding.ASCII.GetBytes(Configuration.GetValue<string>("Secretkey"));
+            var jwt = new JwtSecurityToken(
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(secretKey),
+                    SecurityAlgorithms.HmacSha256Signature),
+                    claims: claims,
+                    expires: expiry,
+                    notBefore: DateTime.UtcNow);
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+    }
+}
